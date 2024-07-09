987ec1b4a72ac5aa1c576761a250e66dca085560
Jose Espejo <joseespejo@yahoo.com>
2024-07-09 14:17:15 +0100

    Initial Commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Jose Espejo on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
