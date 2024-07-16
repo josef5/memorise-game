98314cda36882c073ca4a1816c0e37f59dc0e510
Jose Espejo <joseespejo@yahoo.com>
2024-07-16 18:50:47 +0100

    refactor: remove ScrollView

--- Memorise/EmojiMemoryGameView.swift
+++ Memorise/EmojiMemoryGameView.swift
@@ -13,10 +13,8 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         VStack {
-            ScrollView {
-                cards
-                    .animation(.default, value: viewModel.cards)
-            }
+            cards.animation(.default, value: viewModel.cards)
+
             Button("Shuffle") {
                 viewModel.shuffle()
             }
