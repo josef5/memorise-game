0ea8ac140d3b99f2db3c07156ab0f8a2382ffca1
Jose Espejo <joseespejo@yahoo.com>
2024-07-17 19:35:40 +0100

    refactor: add Pie

--- Memorise/CardView.swift
+++ Memorise/CardView.swift
@@ -25,32 +25,26 @@ struct CardView: View {
             static let smallest = CGFloat(10)
             static let scaleFactor = smallest / largest
         }
+        
+        struct Pie {
+            static let opacity = CGFloat(0.5)
+            static let inset = CGFloat(5)
+        }
     }
     
     var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
-            
-            // Both sides always present so they are not collapsed by LazyVGrid
-            // Face up
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: Constants.lineWidth)
-                Circle().opacity(0.4).overlay(
-                    Text(card.content)
+        Pie(endAngle: .degrees(240))
+            .opacity(0.4)
+            .overlay(
+                Text(card.content)
                     .font(.system(size: Constants.FontSize.largest))
                     .minimumScaleFactor(Constants.FontSize.scaleFactor)
                     .multilineTextAlignment(.center)
                     .aspectRatio(1, contentMode: .fit)
-                    .padding(Constants.inset)
-                )
-                
-            }.opacity(card.isFaceUp ? 1 : 0)
-            
-            // Face down
-            base.fill().opacity(card.isFaceUp ? 0 : 1)
-        }
-        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+                    .padding(Constants.Pie.inset)
+            )
+            .padding(Constants.inset)
+            .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
